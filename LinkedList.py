f3c86ac15046d719c9a077d060e783c771ce8f24
Jake <jacob_dominski@yahoo.com>
2021-02-20 10:49:19 -0700

    Update LinkedList.py
    added some functions for the linked list mostly for displaying values

--- LinkedList.py
+++ LinkedList.py
@@ -1,6 +1,20 @@
 class LinkedList:
   def __init__(self):
     self.head = None
+  
+  def printList(self):
+    temp = self.head
+    while temp:
+      print(temp.data)
+      temp = temp.next
+  
+  def getValue(self, index):
+    temp = self.head
+    
+    for i in range(index):
+      temp = temp.next
+    
+    return temp.data
 
 
 class Node:
