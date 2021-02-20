128a5f0ec1c225b1353f728f742585f02140664d
Jake <jacob_dominski@yahoo.com>
2021-02-20 13:10:00 -0700

    Update main.py
    added main file to use the linked list

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 
+from LinkedList import LinkedList
+from random import randint
+
+linklist = LinkedList()
+
+for i in range(10):
+  linklist.push(randint(0, 20))
+
+
+
+linklist.printList()
+print('------')
+print(linklist.getValue(1))
