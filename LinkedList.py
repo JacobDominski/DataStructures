497b181dd20e847676674dcbee377475721e8ac9
Jake <jacob_dominski@yahoo.com>
2021-02-20 10:50:30 -0700

    Update LinkedList.py
    added functions to link values into the linked list

--- LinkedList.py
+++ LinkedList.py
@@ -15,6 +15,40 @@ class LinkedList:
       temp = temp.next
     
     return temp.data
+  
+  def length(self):
+    temp = self.head
+    count = 0
+
+    while temp:
+      count += 1
+      temp = temp.next
+    
+    return count
+  
+  def push(self, data):
+    newNode = Node(data)
+
+    if self.head is None:
+      self.head = newNode
+      return
+    
+    last = self.head
+    while last.next:
+      last = last.next
+    
+    last.next = newNode
+
+  def insert(self, prevNode, data):
+    if prevNode is None:
+      print("Error, the previous node is not inside the Linked List")
+      return
+    
+    newNode = Node(data)
+
+    newNode.next = prevNode.next
+
+    prevNode.next = newNode
 
 
 class Node:
